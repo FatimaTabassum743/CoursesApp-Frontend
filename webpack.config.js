8aaea87ee79c0bdabc6be693c50a16864efc6666
Fatima Tabassum <fatimatabassum743@gmail.comm>
2026-01-21 22:12:54 +0530

    adding some files

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+    publicPath: "/",
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/_redirects", to: "." }
+      ]
+    })
+  ],
   module: {
     rules: [
       {
