5b66c11cb5eb3f6efdb1fb2367eeaf99eb3e5973
Fatima Tabassum <fatimatabassum743@gmail.comm>
2026-01-21 21:59:05 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://lms-backend-ztnc.onrender.com"
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api"
 });
 
 
