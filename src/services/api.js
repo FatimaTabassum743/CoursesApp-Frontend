6237d860e3cf4d8b2ea6ccaedc0f98bf964d0419
Fatima Tabassum <fatimatabassum743@gmail.comm>
2026-01-21 22:57:44 +0530

    updated backend api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://coursesapp-backend-1.onrender.com/"
+  baseURL:"https://coursesapp-backend-1.onrender.com/api"
 });
 
 
